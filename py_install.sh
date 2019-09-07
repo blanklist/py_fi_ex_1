ef4a77a9e7a5814e61c5ed26ee6a5ad9c904cbdc
blanklist <joehineriv@gmail.com>
2019-09-07 11:18:31 -0700

    pandas and ipython running

--- py_install.sh
+++ py_install.sh
@@ -2,19 +2,27 @@
 #
 # Script to install linux system tools and basic python components.
 #
+GREEN='\033[1;32m'
+NC='\033[0m'
 # GENERAL LINUX
-apt-get update
+printf "${GREEN}GENERAL LINUX{$NC}\n"
+apt-get update && apt-get install -y --no-install-recommends apt-utils
 apt-get upgrade
 # installs system tools
+printf "${GREEN}Install system tools{$NC}\n"
 apt-get install -y bzip2 gcc git htop screen vim wget
-apt-get upgrade -y #!/usr/bin/env bash
+apt-get upgrade -y bash
 apt-get clean
 
 # INSTALL MINICONDA
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O \Miniconda.sh
+printf "${GREEN}INSTALL MINICONDA{$NC}\n"
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh
 bash Miniconda.sh -b
 rm -rf Miniconda.sh
 export PATH="/root/miniconda3/bin:$PATH"
 
 # PYTHON LIBRARIES
-conda update -y conda python pandas ipython
+printf "${GREEN}PYTHON LIBRARIES{$NC}\n"
+conda update -y conda python
+conda install -y pandas
+conda install -y ipython
